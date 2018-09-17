5233f59ba631e0bc93b9a2277334e598533be053
darisborel5 <darisborel5@gmail.com>
2018-09-17 13:50:15 -0400

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  get 'welcome/index'
+  
+  root 'welcome on rails ruby bdaris'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
