0df7aa4a876fcb17bf4c3a62104b2b18025b3c48
darisborel5 <darisborel5@gmail.com>
2018-09-20 18:43:00 -0400

    message de commit

--- config/puma.rb
+++ config/puma.rb
@@ -33,6 +33,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-#run rails db:migrate before deploying application
-web bundle exec puma -C config/puma.rb 
+#run rails db:migrate before deploying application 
 release: rails db:migrate
