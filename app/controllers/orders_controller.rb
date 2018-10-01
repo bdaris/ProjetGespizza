94b14ba9a2bb0cbd17a1fe54dbbdf05962d39b7e
darisborel5 <darisborel5@gmail.com>
2018-10-01 03:15:33 -0400

    message de commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,13 @@
-class OrdersController < ApplicationController\
+class OrdersController < ApplicationController
   resources :pizzas, :only => [:show, :create, :update, :destroy]
   resources :orders, :only => [:index]
+  
+  def index
+     @orders = order.all
+  end
+  
+  def order_params
+      params.require(:order).permit(:name, :address, :url)
+  end
 end
+
